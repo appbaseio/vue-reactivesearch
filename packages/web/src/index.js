9824e1629870f559362e7950f009a9bfabd23c0b
kuldeep <kuldepsaxena155@gmail.com>
2018-10-19 17:22:55 +0530

    feat(): Added SingleRange Component

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -3,9 +3,10 @@ import ReactiveBase from './components/ReactiveBase/index.jsx';
 import DataSearch from './components/search/DataSearch.jsx';
 import SingleList from './components/list/SingleList.jsx';
 import MultiList from './components/list/MultiList.jsx';
+import SingleRange from './components/range/SingleRange.jsx';
 import version from './components/Version/index';
 
-const components = [ReactiveList, ReactiveBase, DataSearch, SingleList, MultiList];
+const components = [ReactiveList, ReactiveBase, DataSearch, SingleList, MultiList, SingleRange];
 
 const install = function (Vue) {
   components.map((component) => {
@@ -18,7 +19,16 @@ if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue);
 }
 
-export { version, install, ReactiveList, ReactiveBase, DataSearch, SingleList, MultiList };
+export {
+  version,
+  install,
+  ReactiveList,
+  ReactiveBase,
+  DataSearch,
+  SingleList,
+  MultiList,
+  SingleRange,
+};
 
 export default {
   version,
